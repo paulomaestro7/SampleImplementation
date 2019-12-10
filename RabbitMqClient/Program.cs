26683b8a9cba42a665f820f8164fd2f505b9eb88
Maestro <paulomaestro@employer.com.br>
2019-12-09 23:47:25 -0300

    Add project files.

---
+++ RabbitMqClient/Program.cs
@@ -0,0 +1,43 @@
+﻿using RabbitMQ.Client;
+using System;
+using System.Text;
+
+namespace RabbitMqClient
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var factory = new ConnectionFactory() { HostName = "localhost", UserName = "guest", Password = "guest", Port = 5672 };
+            var connection = factory.CreateConnection();
+            var channel = connection.CreateModel();
+
+            var queue = "task_queue";
+
+            channel.QueueDeclare(queue: queue, durable: true, exclusive: false, autoDelete: false, arguments: null);
+
+            Console.WriteLine("Start send message");
+
+            while (true)
+            {
+                Console.WriteLine("");
+                Console.WriteLine("Write the message");
+
+                var text = Console.ReadLine();
+                Console.Clear();
+
+                var body = Encoding.UTF8.GetBytes(text);
+
+                var properties = channel.CreateBasicProperties();
+                properties.Persistent = true;
+
+                channel.BasicPublish(exchange: "",
+                                     routingKey: queue,
+                                     basicProperties: properties,
+                                     body: body);
+
+                Console.WriteLine("Send: " + text);
+            }
+        }
+    }
+}
---
+++ RzbbitMqWork/Program.cs
@@ -0,0 +1,47 @@
+﻿using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System;
+using System.Text;
+using System.Threading;
+
+namespace RabbitMqWork
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var factory = new ConnectionFactory() { HostName = "localhost", UserName = "guest", Password = "guest", Port = 5672 };
+            var connection = factory.CreateConnection();
+            var channel = connection.CreateModel();
+
+            var queue = "task_queue";
+
+            channel.QueueDeclare(queue: queue, durable: true, exclusive: false, autoDelete: false, arguments: null);
+
+            channel.BasicQos(prefetchSize: 0, prefetchCount: 1, global: false);
+
+            Console.WriteLine("Awaiting messages");
+
+            var consumer = new EventingBasicConsumer(channel);
+            consumer.Received += (model, ea) =>
+            {
+                var body = ea.Body;
+                var message = Encoding.UTF8.GetString(body);
+                Console.WriteLine("Received:  " + message);
+
+                int dots = message.Split('.').Length - 1;
+                Thread.Sleep(dots * 1000);
+
+                Console.WriteLine("Done");
+
+                channel.BasicAck(deliveryTag: ea.DeliveryTag, multiple: false);
+            };
+            channel.BasicConsume(queue: queue,
+                                 autoAck: false,
+                                 consumer: consumer);
+
+            Console.WriteLine("Pressione [Enter] para sair.");
+            Console.ReadLine();
+        }
+    }
+}
